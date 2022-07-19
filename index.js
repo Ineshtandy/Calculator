17eb2e4b21bcb3bf3777a4ff6f63613522263f4a
Inesh <i08polymath@gmail.com>
2022-07-19 16:48:06 +0530

    JAVASCRIPT UPDATE added store function and an array which stores the values sent from onclick of the buttons in html

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+// let oneBtn = document.querySelector("#one");
+// oneBtn.addEventListener("click",e => {
+//     console.log(e.innerText);
+// });
+// let twoBtn = document.querySelector("#2");
+// let threeBtn = document.querySelector("#3");
+// let fourBtn = document.querySelector("#4");
+// let fiveBtn = document.querySelector("#5");
+// let sixBtn = document.querySelector("#6");
+// let sevenBtn = document.querySelector("#7");
+// let eightBtn = document.querySelector("#8");
+// let nineBtn = document.querySelector("#9");
+
+// let addBtn = document.querySelector("#+");
+// let minusBtn = document.querySelector("#-");
+// let multiplyBtn = document.querySelector("#*");
+// let divideBtn = document.querySelector("#/");
+// let equalBtn = document.querySelector("#=");
+// let clearBtn = document.querySelector("#clear");
+
+let arr = [];
+function store(val){
+    //console.log(val);
+    arr.push(val);
+    console.log(arr);
+
+    //let counter = 0;
+    // let arr[3];
+    // if(counter < 3){
+    //     arr[counter] = val;
+    // }else{
+    //     //call function
+    // }
+}
 
 function add(n1 ,n2){
     console.log(n1+n2);
